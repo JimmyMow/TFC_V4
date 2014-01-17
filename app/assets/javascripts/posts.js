e0aeb632c320b95371454e61964e9bdeef2dc896
Jack Mallers <jimmymowschess@gmail.com>
2014-01-16 18:07:12 -0600

    before i add delayed_jobs to mailers

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,13 +1,13 @@
-// $(document).ready(function(){
-//     var path = $(location).attr('href')
-//     console.log(path);
-//     $('h2 a').each(function() {
-//         if( path === "http://localhost:3000/posts?new_post=true" ){
-//             $("#most_recent").addClass('active');
-//         } else {
-//             $('#most_popular').addClass('active');
-//         }
-//     });
-// });
-
+$(document).ready(function(){
+    var path = $(location).attr('href')
+    console.log(path);
+    $('h2 a').each(function() {
+        if( path === "http://localhost:3000/posts?new_post=true" ){
+            $("#most_recent").addClass('active');
+        } else {
+            $('#most_popular').addClass('active');
+        }
+    });
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+});
 
