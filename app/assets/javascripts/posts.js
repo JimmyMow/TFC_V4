11a7e629a968fe648b21e6e52bad0b65338fe996
Jack Mallers <jimmymowschess@gmail.com>
2014-01-17 15:58:59 -0600

    fixed javascript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     var path = $(location).attr('href')
     console.log(path);
-    $('h2 a').each(function() {
+    $('h3 a').each(function() {
         if( path === "http://localhost:3000/posts?new_post=true" ){
             $("#most_recent").addClass('active');
-        } else {
+        } else if(path === "http://localhost:3000/posts" ) {
             $('#most_popular').addClass('active');
         }
     });
