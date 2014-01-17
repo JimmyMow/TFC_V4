3a2a3f2bab26336093c97cdbeabe3040a7d451f4
Jack Mallers <jimmymowschess@gmail.com>
2014-01-17 16:28:26 -0600

    fixing javascript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
     var path = $(location).attr('href')
     console.log(path);
     $('h3 a').each(function() {
-        if( path === "http://localhost:3000/posts?new_post=true" ){
+        if( path === "http://thefanschallenge.herokuapp.com/posts?new_post=true" ){
             $("#most_recent").addClass('active');
-        } else if(path === "http://localhost:3000/posts" ) {
+        } else if(path === "http://thefanschallenge.herokuapp.com/posts" ) {
             $('#most_popular').addClass('active');
         }
     });
-    $('h3 a').on('click', function() {
-      alert('hey');
-    });
+    // $('h3 a').on('click', function() {
+    //   alert('hey');
+    // });
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 });
 
