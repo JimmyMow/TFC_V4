1ea7c6cd5c8880b0c0ed8e81ed3c6df14a469d28
Jack Mallers <jimmymowschess@gmail.com>
2014-01-19 17:08:30 -0600

    Added mobile logic/mime_type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :prepare_for_mobile
 
   protected
 
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+  helper_method :mobile_device?
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
